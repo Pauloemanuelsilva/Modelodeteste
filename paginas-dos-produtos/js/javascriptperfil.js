ffb180a21b120ca37ad220fd8c7b3271e6243d86
Pauloemanuelsilva <smariaclarice8@gmail.com>
2025-07-22 11:42:34 -0300

    modificação nas paginas9

---
+++ paginas-dos-produtos/js/javascriptperfil.js
@@ -0,0 +1,17 @@
+const inputFoto = document.getElementById('input-foto');
+const avatar = document.getElementById('avatar');
+document.querySelector('.avatar-btn').addEventListener('click', () => inputFoto.click());
+
+inputFoto.addEventListener('change', e => {
+  const file = e.target.files[0];
+  if (!file) return;
+  const reader = new FileReader();
+  reader.onload = ev => {
+    avatar.src = ev.target.result;
+    localStorage.setItem('perfilAvatar', ev.target.result);
+  };
+  reader.readAsDataURL(file);
+});
+
+const savedAvatar = localStorage.getItem('perfilAvatar');
+if (savedAvatar) avatar.src = savedAvatar;
